03c53960d4de4b673732eb768fdbfd745ce366ba
cbowen <cbowen@cse.msu.edu>
2019-01-06 13:01:27 -0500

    Removed some unused files. New cl-publish system.

---
+++ publish.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = {
+	dist: path.resolve(__dirname, 'dist'),
+	files: [
+		'Cirsim\\.js', '^cirsim/'
+	]
+}
--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = {
 	entry: {
@@ -23,14 +22,6 @@ module.exports = {
 				to: 'cirsim/help',
 				flatten: false
 			}
-		]),
-		new FileManagerPlugin({
-			onEnd: {
-				copy: [
-					{source: path.resolve(__dirname, '../../../cl/dist/cirsim.*'), destination: path.resolve(__dirname, 'dist') },
-					{source: path.resolve(__dirname, '../../../cl/dist/cirsim/'), destination: path.resolve(__dirname, 'dist/cirsim') }
-				]
-			}
-		})
+		])
 	]
 }
